f3c693bca7dc1c234b0645c5c2701f3301a4d887
dagu <dagu0925@gmail.com>
2020-10-24 23:15:03 +0800

    init

---
+++ mifu-core/src/main/java/org/dagu/mifu/core/Application.java
@@ -0,0 +1,15 @@
+package org.dagu.mifu.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dagu
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ mifu-core/src/main/java/org/dagu/mifu/core/dao/entity/SpaceDo.java
@@ -0,0 +1,9 @@
+package org.dagu.mifu.core.dao.entity;
+
+/**
+ * @author dagu
+ * @date 2020/10/5 8:30 下午
+ * @desc 空间表数据实体
+ */
+public class SpaceDo {
+}
